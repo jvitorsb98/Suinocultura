4f096aba60905cb69b541bd2904771e6a59de021
joao ramos <jvitorsb98@outlook.com>
2024-03-09 23:22:13 -0300

    conectando ao branco de dados

--- Suinocultura-dan/suinocultura/src/app/components/pesagem-suinos/pesagem-suinos.component.ts
+++ Suinocultura-dan/suinocultura/src/app/components/pesagem-suinos/pesagem-suinos.component.ts
@@ -1,56 +1,42 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { Chart } from 'chart.js/auto';
-import { SuinoPesos } from '../../model/suino'
-
-
-const SUINOS: SuinoPesos[] = [
-  { peso: 55, dt_pesagem: '2020-06-05', brinco_suino: "1" },
-  { peso: 60, dt_pesagem: '2020-07-05', brinco_suino: "1" },
-  { peso: 65, dt_pesagem: '2020-08-05', brinco_suino: "1" },
-  { peso: 70, dt_pesagem: '2020-09-05', brinco_suino: "1" },
-  { peso: 68, dt_pesagem: '2020-10-05', brinco_suino: "1" },
-  { peso: 72, dt_pesagem: '2020-11-05', brinco_suino: "1" },
-  { peso: 80, dt_pesagem: '2020-12-05', brinco_suino: "1" },
-  { peso: 78, dt_pesagem: '2021-01-05', brinco_suino: "1" },
-  { peso: 85, dt_pesagem: '2021-02-05', brinco_suino: "1" },
-  { peso: 82, dt_pesagem: '2021-03-05', brinco_suino: "1" },
-
-  { peso: 58, dt_pesagem: '2022-06-05', brinco_suino: "2" },
-  { peso: 63, dt_pesagem: '2022-07-05', brinco_suino: "2" },
-  { peso: 66, dt_pesagem: '2022-08-05', brinco_suino: "2" },
-  { peso: 72, dt_pesagem: '2022-09-05', brinco_suino: "2" },
-  { peso: 70, dt_pesagem: '2022-10-05', brinco_suino: "2" },
-
-  { peso: 50, dt_pesagem: '2022-06-10', brinco_suino: "3" },
-  { peso: 55, dt_pesagem: '2022-07-10', brinco_suino: "3" },
-  { peso: 60, dt_pesagem: '2022-08-10', brinco_suino: "3" },
-  { peso: 65, dt_pesagem: '2022-09-10', brinco_suino: "3" },
-  { peso: 68, dt_pesagem: '2022-10-10', brinco_suino: "3" },
-
-  { peso: 62, dt_pesagem: '2022-06-15', brinco_suino: "4" },
-  { peso: 68, dt_pesagem: '2022-07-15', brinco_suino: "4" },
-  { peso: 70, dt_pesagem: '2022-08-15', brinco_suino: "4" },
-  { peso: 75, dt_pesagem: '2022-09-15', brinco_suino: "4" },
-  { peso: 80, dt_pesagem: '2022-10-15', brinco_suino: "4" }
-];
+import { DatabaseService } from '../../services/database.service';
+import { Suino } from '../../model/suino';
 
 @Component({
   selector: 'app-pesagem-suinos',
   templateUrl: './pesagem-suinos.component.html',
   styleUrls: ['./pesagem-suinos.component.css']
 })
-export class PesagemSuinosComponent {
+export class PesagemSuinosComponent implements OnInit {
   @ViewChild("graficoSuinos") elemento!: ElementRef;
 
   brincoSuino: string = "";
   suinoEncontrado: boolean = false;
-  suinoSelecionado: SuinoPesos[] = [];
+  suinoSelecionado: Suino | null = null;
+
+  constructor(private databaseService: DatabaseService) { }
+
+  ngOnInit() {
+    this.buscarSuino();
+  }
 
   buscarSuino() {
-    this.suinoSelecionado = SUINOS.filter(suino => suino.brinco_suino === this.brincoSuino);
-    this.suinoEncontrado = this.suinoSelecionado.length > 0;
-    if (this.suinoEncontrado)
-      this.renderizarGrafico();
+    this.databaseService.getSuinoPorBrinco(this.brincoSuino)
+      .subscribe(
+        suino => {
+          this.suinoSelecionado = suino;
+          this.suinoEncontrado = suino !== null;
+          if (this.suinoEncontrado) {
+            this.renderizarGrafico();
+          }
+        },
+        error => {
+          console.error("Erro ao buscar o suíno:", error);
+          this.suinoEncontrado = false;
+          this.suinoSelecionado = null;
+        }
+      );
   }
 
   renderizarGrafico() {
@@ -59,8 +45,8 @@ export class PesagemSuinosComponent {
       return;
     }
 
-    const pesos = this.suinoSelecionado.map(suino => suino.peso);
-    const datas = this.suinoSelecionado.map(suino => suino.dt_pesagem);
+    const pesos = this.suinoSelecionado!.pesos.map(peso => peso.peso);
+    const datas = this.suinoSelecionado!.pesos.map(peso => peso.dt_pesagem);
 
     new Chart(this.elemento.nativeElement, {
       type: 'bar',
