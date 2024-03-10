f3de0bed9d4cba3eafca4cc10ee68cc0d0d69cc1
joao ramos <jvitorsb98@outlook.com>
2024-03-10 17:50:15 -0300

    Organizando repositorio

--- Suinocultura-dan/suinocultura/src/app/pipes/idade.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IdadePipe } from './idade.pipe';
-
-describe('IdadePipe', () => {
-  it('create an instance', () => {
-    const pipe = new IdadePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
